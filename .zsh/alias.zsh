88ddbfbfb0359bc5e29950fbe7dffc5df31118d9
mass8 <29457120+h64m1@users.noreply.github.com>
2020-05-02 18:35:03 +0900

    [add] add git command alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -6,3 +6,8 @@ alias mkdir="mkdir -p"
 alias pd="pushd ; dirs -v "
 alias po="popd ; dirs -v"
 alias chrome_local="open -a Google\ Chrome --args -allow-file-access-from-files"
+
+# git
+alias git_rm_remote_branch="git push orign :[remote branch name]"
+alias git_sync_branch="git remote prune origin"
+
