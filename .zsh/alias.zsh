7e2e252d1215e7bf090f25b458a81652a24a105e
mass8 <29457120+h64m1@users.noreply.github.com>
2020-08-24 23:59:06 +0900

    Add prettier default configuration

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -11,3 +11,10 @@ alias chrome_local="open -a Google\ Chrome --args -allow-file-access-from-files"
 alias git_rm_remote_branch="git push orign :[remote branch name]"
 alias git_sync_branch="git remote prune origin"
 
+# copy prettier configuration file to current directory
+copy-prettierrc() {
+  cp -v $HOME/.prettierrc .
+  echo "prettier configuration ..."
+  cat .prettierrc
+}
+
