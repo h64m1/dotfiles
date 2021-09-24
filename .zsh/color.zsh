5e3e4feac0b9ca50b12c61297dda36e9430c14a1
hmasui <29457120+h64m1@users.noreply.github.com>
2021-09-24 23:56:00 +0900

    clone if color file does not exist

--- .zsh/color.zsh
+++ .zsh/color.zsh
@@ -1,4 +1,7 @@
 #_______________________________________________________________________________
 # ls color settings
+if [ ! -f ~/.dircolors.256dark ]; then
+   git clone https://github.com/seebi/dircolors-solarized/blob/master/dircolors.256dark ~/.dircolors.256dark
+fi
 eval $(dircolors ~/.dircolors.256dark)
 
