643850c204bcdba9e011d36d03cabf23f8e59c08
hmasui <29457120+h64m1@users.noreply.github.com>
2021-12-09 20:06:21 +0900

    add padding to alacritty
    - add ignore setting for .config
    - vi alias -> vim

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -6,7 +6,7 @@ alias mkdir="mkdir -p"
 alias pd="pushd ; dirs -v "
 alias po="popd ; dirs -v"
 alias chrome_local="open -a Google\ Chrome --args -allow-file-access-from-files"
-alias vi='nvim'
+alias vim='nvim'
 
 # git
 alias git_rm_remote_branch="git push orign :[remote branch name]"
